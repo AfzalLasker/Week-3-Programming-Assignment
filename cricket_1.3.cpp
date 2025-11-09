42ded1430d2ebc58a0fbbb137952194c367a135f
AfzalLasker <afzallasker98@gmail.com>
2025-11-09 16:43:40 -0500

    Add Cricket Quiz version 1.3 program

---
+++ cricket_1.3.cpp
@@ -0,0 +1,224 @@
+// Program: Cricket Quiz
+// Description: This program runs a short cricket quiz that asks the user four questions related to cricket. It checks answers,
+// counts the correct answers, and shows the final score and percentage.
+
+// Afzal Lasker
+
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    const int total_question = 4;
+
+    const double ticket_total_cost = 21.98;
+
+    const int min_answer_2 = 2;
+    const int max_answer_2 = 6;
+
+    const int min_answer_3 = 5;
+    const int max_answer_3 = 8;
+
+    const int correct_answer_4 = 2;
+
+    string user_name;
+    double ticket_cost;
+    double answer_2;
+    double answer_3;
+    double answer_4;
+    double total_score;
+    total_score = 0;
+
+
+
+    // Welcome message
+    cout << "Welcome to the Cricket Quiz!" << endl << endl;
+
+
+
+    // Get user name
+    cout << "What is your name? ";
+    getline(cin, user_name);
+    cout << endl;
+
+    cout << "Hello! " << user_name << ", Let's get started" << endl << endl;
+
+
+
+    // Question 1
+    cout << "Question no. 1\n"
+         << "If a cricket match ticket price is $10.99, then for two people how much will it cost?\n"
+         << "Enter your answer (must include decimals): ";
+
+    cin >> ticket_cost;
+    // Input check for wrong data type
+    if (cin.fail())             // Check if user entered the wrong input type
+    {
+        cin.clear();            // Clear the error state
+        cin.ignore(100, '\n');  // remove the wrong input from memory
+        cout << "Invalid input! Please enter a number with a decimal point." << endl << endl;
+    }
+    else if (ticket_cost == ticket_total_cost)
+    {
+        total_score++;
+        cout << endl;
+        cout << "Perfect! Now move on to the next question." << endl << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << "Wrong! The correct answer is $" << ticket_total_cost << " \n"
+                "Now move on to the next question." << endl << endl;
+    }
+
+
+
+    // Question 2
+    cout << "Question no. 2 \n"
+            "If 6 overs are left (1 over = 6 balls) and 40 runs are needed to win, \n"
+            "how many overs are at least needed to win if every ball can score up to 6 runs?\n\n"
+            "Give your answer in number: ";
+
+    cin >> answer_2;
+    // Input check for wrong data type
+    if (cin.fail())             // Check if user entered the wrong input type
+    {
+        cin.clear();           // Clear the error state
+        cin.ignore(100, '\n');  // remove the wrong input from memory
+        cout << "Invalid input! Please answer using a number." << endl << endl;
+    }
+    else if (answer_2 >= min_answer_2 && answer_2 <= max_answer_2)
+    {
+        total_score++;
+        cout << endl;
+        cout << "Perfect! Now move on to the next question." << endl << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << "Wrong! Good luck with the next question." << endl << endl;
+    }
+
+
+
+    // Question 3
+    cout << "Question no. 3 \n"
+            "If 6 overs are left (1 over = 6 balls) and 180 runs are needed to win,\n"
+            "how many overs are at least needed to win if every ball can score up to 6 runs?\n\n"
+            "Give your answer in number: ";
+
+    cin >> answer_3;
+    // Input check for wrong data type
+    if (cin.fail())             // Check if user entered the wrong input type
+    {
+        cin.clear();            // Clear the error state
+        cin.ignore(100, '\n');  // remove the wrong input from memory
+        cout << "Invalid input! Please answer using a number." << endl << endl;
+    }
+    else if (answer_3 >= min_answer_3 && answer_3 <= max_answer_3)
+    {
+        total_score++;
+        cout << endl;
+        cout << "Perfect! Now move on to the last question." << endl << endl;
+    }
+    else
+    {
+        cout << endl;
+        cout << "Wrong! Good luck with the last question." << endl << endl;
+    }
+
+
+
+    // Question 4
+    cout << "Question no. 4 \n"
+            "If 2 overs are left (1 over = 6 balls) and 42 runs are needed to win,\n"
+            "how many overs are at least needed to win if every ball can score up to 6 runs?\n\n"
+            "Give your answer in number: ";
+
+    cin >> answer_4;
+    // Input check for wrong data type
+    if (cin.fail())             // Check if user entered the wrong input type
+    {
+        cin.clear();            // Clear the error state
+        cin.ignore(100, '\n');  // remove the wrong input from memory
+        cout << "Invalid input! \n\n"
+                "Game Over" << endl << endl;
+    }
+    else if (answer_4 == correct_answer_4)
+    {
+        total_score++;
+        cout << "Great! \n\n"
+                "Game Over" << endl << endl;
+    }
+    else
+    {
+        cout << "\nWrong Answer! \n\n"
+                "*** Game Over ***" << endl << endl;
+    }
+
+
+
+    // Calculate percentage
+    double percentage = (total_score / total_question) * 100;
+
+
+
+
+    // Display a simple menu using switch to show quiz results based on user choice
+    int choice;
+    cout << "Menu:\n";
+    cout << "1. View correct answer ratio" << endl;
+    cout << "2. View Score Only" << endl;
+    cout << "3. View How Many Answers Were Wrong" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1:
+            cout << "Your correct answer ratio is " << percentage << "%" << endl << endl;
+            break;
+        case 2:
+            cout << "You answered " << total_score << " questions correctly." << endl << endl;
+            break;
+        case 3:
+            cout << "You got " << (total_question - total_score) << " questions wrong." << endl << endl;
+            break;
+        default:
+            cout << "Invalid choice." << endl << endl;
+            break;
+    }
+
+
+    // Create the final output of the program
+    cout << "Cricket Quiz Score Report for " << user_name << endl;
+    cout << "------------------------------------" << endl <<endl;
+    cout << left << setw(30) << "Total questions are" << right << setw(6) << total_question << endl << endl;
+    cout << left << setw(30) << "Your total correct answers: " << right << setw(6) << total_score << endl << endl;
+    cout << left << setw(30) << "Your correct answer ratio: " << right << setw(6) << setprecision(2) << fixed << percentage << "%" << endl << endl;
+
+
+    // Save the report to "Report.txt" file
+    ofstream report("Week3_report.txt");
+    report << "Cricket Quiz Score Report for " << user_name << endl;
+    report << "------------------------------------" << endl << endl;
+    report << left << setw(30) << "Total questions are" << right << setw(6) << total_question << endl << endl;
+    report << left << setw(30) << "Total correct answers: " << right << setw(6) << total_score << endl << endl;
+    report << left << setw(30) << "Correct answer ratio: " << right << setw(6) << setprecision(2) << fixed << percentage << "%" << endl;
+    report.close();
+
+
+    // Show a confirmation that the report was saved
+    cout << "\n### Report saved successfully to report.txt file" << endl << endl;
+
+    return 0;
+}
+
+
